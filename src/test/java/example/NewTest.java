ef8165bfe8bed477ee3851d46ab3c925341b006a
rivi <rivi@rivi-PC.int.lemontree.se>
2017-03-31 12:53:10 +0200

    Added Mercury Tours code

--- src/test/java/example/NewTest.java
+++ src/test/java/example/NewTest.java
@@ -1,6 +1,8 @@
 package example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
@@ -12,14 +14,28 @@ public class NewTest {
 	private WebDriver driver;
 
 	@Test
-	public void testEasy() {
-		driver.get("http://demo.guru99.com/selenium/guru99home/");
-		String title = driver.getTitle();				 
-		Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
+	public void testEasy() throws InterruptedException {
+//		driver.get("http://demo.guru99.com/selenium/guru99home/");
+//		String title = driver.getTitle();				 
+//		Assert.assertTrue(title.contains("Demo Guru99 Page")); 	
+		driver.navigate().to("http://newtours.demoaut.com/");
+		String strPageTitle = driver.getTitle();
+		Thread.sleep(3000);
+		System.out.println("Page title is: " + strPageTitle);
+		Assert.assertTrue(strPageTitle.equalsIgnoreCase("Welcome: Mercury Tours"), "Page title does not match");
+		WebElement element = driver.findElement(By.xpath("//input[(@name='userName')]"));
+		element.sendKeys("mercury");
+		 driver.findElement(By.name("password")).sendKeys("mercury");
+
+		// Click on login button
+		driver.findElement(By.name("login")).click();
+		Thread.sleep(5000);
+		String strPageTitle1 = driver.getTitle();
+		System.out.println("Login is successful: " + strPageTitle1);
 	}
 
 	@BeforeTest
-	public void beforeTest() {
+	public void beforeTest() throws InterruptedException {
 	String Browsertype = "Chrome1";
 	if (Browsertype == "Chrome" ) {
 
@@ -31,6 +47,7 @@ public class NewTest {
 			System.setProperty("webdriver.gecko.driver","C://geckodriver.exe");
 			driver = new FirefoxDriver();
 			System.out.println("Firefox browser is started");
+			
 		}
 	}
 
