f327e5f7a5941771f0269da0a0714d25575a98fd
rivi <rivi@rivi-PC.int.lemontree.se>
2017-05-12 13:53:52 +0200

    Added as Chrome1

--- src/test/java/example/NewTest.java
+++ src/test/java/example/NewTest.java
@@ -49,7 +49,7 @@ public class NewTest {
 	
 	@BeforeTest
 	public void beforeTest() throws InterruptedException {
-	String Browsertype = "Chrome";
+	String Browsertype = "Chrome1";
 	if (Browsertype == "Chrome" ) {
 
 			System.setProperty("webdriver.chrome.driver","chromedriver.exe");
